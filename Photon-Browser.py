570c3f4194afd1cf81f39ec2f4d2957be8c68e15
N3koSempai <alvarom12098@gmail.com>
2022-02-03 02:44:50 -0500

    make webview into a tab.
    function to add new tab not available yet

--- Photon-Browser.py
+++ Photon-Browser.py
@@ -6,7 +6,8 @@ gi.require_version('Gtk', '3.0')
 gi.require_version('WebKit2', '4.0')
 from gi.repository import Gtk
 from gi.repository.WebKit2 import WebView, Settings
-import re
+from gi.repository import WebKit2
+import re, os
 
 
 class Handler:
@@ -14,36 +15,54 @@ class Handler:
         Gtk.main_quit()
 
     def searching(self, search):
-        url = search.get_text()
-
+        url = mainwindow.search.get_text()
+        #different behavior depending on if you have http: or not at the beginning
         if re.search(r"^http://*", url):
-            webview.load_uri(url)
+            mainwindow.webview.load_uri(url)
             print('ok')
+        elif re.search(r"^about:", url):
+            if url == 'about:history':
+                mainwindow.webview.load_uri('file:///var/')
         else:
-            print(['with engine'])
-            webview.load_uri((Engine % (url)))
+            print('with engine')
+            mainwindow.webview.load_uri((Engine % (url)))
+            print('engine second part')
         fhand = open('History', 'a')
         fhand.write(url + '\n')
-#call builder for glade
-builder = Gtk.Builder()
-builder.add_from_file('graphic.glade')
-#call objects in the glade template
-webview = builder.get_object('Webview')
-window = builder.get_object('MainWindow')
-search = builder.get_object('Searchbox')
+        fhand.close()
 
-#default size of all window
-window.set_default_size(900,500)
-window.set_title('Photon Browser')
+class MainWindow:
+
+    def __init__(self):
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file('graphic.glade')
+        self.webview = self.builder.get_object('Webview')
+        self.window = self.builder.get_object('MainWindow')
+        self.window.set_default_size(900,500)
+        self.window.set_title('Photon Browser')
+        self.search = self.builder.get_object('Searchbox')
+        self.tabsbar = self.builder.get_object('tabsbar')
+
+
+
+
+#call builder for glade
+#builder = Gtk.Builder()
+#builder.add_from_file('graphic.glade')
+#call objects in the glade template
+
+
 #instance the logic in the class Handler
+mainwindow = MainWindow()
+
 handler = Handler()
 
 #signals
-search.connect('activate', handler.searching)
-window.connect('destroy', handler.onDestroy)
+mainwindow.search.connect('activate', handler.searching)
+mainwindow.window.connect('destroy', handler.onDestroy)
 Engine = 'https://www.ecosia.org/search?q=%s'
 
 
 if __name__ == "__main__":
-    window.show_all()
+    mainwindow.window.show_all()
     Gtk.main()
