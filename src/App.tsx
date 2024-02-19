55946cd76814f731fa9497c0c0a0e7614fb5435c
N3koSempai <alvarom12098@gmail.com>
2024-02-19 01:10:14 -0500

    [feat] visual improve and iframe improve

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,62 @@
-/* <div id='WebViewFramer' className="flex flex-col h-[50%] w-full mt-4 overflow-x-hidden overflow-y-scroll" >
-
-</div> */
-import { Button, Input } from "@material-tailwind/react";
-import "./App.css";
-
+import { Button, Input,MenuHandler, Menu, MenuItem, MenuList } from "@material-tailwind/react";
+import "./App.css"
 import { useState } from "react"
-import Frame from "./components/frame"
+
 function App() {
-  const [searchValue, setSearchValue] = useState('https://www.wikipedia.com')
+  const [searchValue, setSearchValue] = useState('https://www.youtube.com')
   const [fixedSearch, setFixedSearch] = useState('')
 
 
-  return (
-    <div className="container flex flex-col" style={{width: '100%'  ,height: '100vh'}}>
-      <div className="flex w-[100%] h-[10%] items-center justify-center" >
+const fixedSearchFormat = () => {
+  const httpFormat = /^http:\/\//
+  const httpsFormat = /^https:\/\//
+  const googleQuery = 'https://www.google.com/search?igu=1&ei=&q='
+  if (httpFormat.test(searchValue) || httpsFormat.test(searchValue)) {
+    setFixedSearch(searchValue)
+  } else {
+setFixedSearch(`${googleQuery}${searchValue}` )
+  }
 
-    <div className="flex w-[50%] flex-row  items-center justify-center text-center">
-      <Input value={searchValue} onChange={(e) => setSearchValue(e.target.value)} size="lg" variant="outlined"  placeholder="Search" id='searchBard' crossOrigin={''}/>
-      </div>
-      <Button placeholder='' color="blue" className='ml-2'onClick={()=>{setFixedSearch(searchValue)}}>search </Button>
-      </div>
-      <Frame src={fixedSearch} />
-      
-    </div>
-  );
 }
 
-export default App;
+  return (
+<div className=" flex flex-col" style={{width: '100%'  ,height: '100vh'}}>
+  
+  <div className="NavBar flex flex-row items-center mt-2 w-full lg:h-[5%] h-[10%] justify-center" >
+  <div className="self-start w-[30%]" />
+    <div className="flex w-[50%] flex-row  items-center justify-center  ">
+         <Input value={searchValue} onChange={(e) => setSearchValue(e.target.value)} size="lg" variant="outlined"  placeholder="Search" id='searchBard' crossOrigin={''}/>
+         <Button placeholder='' color="blue" className='ml-2'onClick={()=>{fixedSearchFormat()}}>search </Button>
+    </div>
+    <div className="w-[25%]"/>
+    <div className="NavBarMenu  self-center justify-center items-center mr-2 ">
+      <Menu>
+        <MenuHandler>
+        <Button placeholder=''>Menu</Button>
+      </MenuHandler>
+      <MenuList placeholder='' className="items-center justify-center">
+        <MenuItem className="text-center" placeholder=''>Preference</MenuItem>
+        <MenuItem className="text-center" placeholder=''>style</MenuItem>
+        <MenuItem className="text-center" placeholder=''>about me</MenuItem>
+      </MenuList>
+      </Menu>
+    </div>
+    
+    
+    
+  </div>
+  <div className="w-full h-full  mt-2" style={{ borderRadius: '1.5rem 1.5rem 0 0', borderImage: 'linear-gradient(to right, blue, red) 1', borderWidth: '0.4rem 0 0 0', borderStyle: 'solid'  }}>
+
+    <iframe title="Main Webview" 
+  sandbox="allow-modals allow-top-navigation-by-user-activation allow-same-origin allow-scripts allow-popups allow-forms"
+   loading="lazy" referrerPolicy="unsafe-url"
+  src={fixedSearch} className="w-full h-full" />
+      </div> 
+</div>
+  )
+}
+
+export default App
+
+
+
